45ecb6743792093fbf3d9e89f997c388c0554fca
Rejoice M. Rabino <rejoicerabino@gmail.com>
2025-01-11 22:06:25 +0800

    Creation of UI for the job hunt application section of the employee side

--- user/userJobView.php
+++ user/userJobView.php
@@ -10,7 +10,7 @@
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <link rel="stylesheet" href="../assets/css/userJobView.css">
+  <link rel="stylesheet" href="../assets/css/userCSS/userJobView.css">
   <link rel="icon" href="../assets/image/stbLogo.ico">
 </head>
 
@@ -63,7 +63,8 @@
   <?php include "../assets/shared/footer.php" ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+    crossorigin="anonymous">
+  </script>
 </body>
 
 </html>
