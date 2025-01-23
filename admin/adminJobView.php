be4f2b0754f8901f91dd8e09978b430ca4164fa4
Althea Algaba <altheaalgaba16@gmail.com>
2025-01-23 15:50:35 +0800

    Modularize JobView function on admin side

--- admin/adminJobView.php
+++ admin/adminJobView.php
@@ -1,44 +1,5 @@
 <?php
-
-include("../connect.php");
-
-if (isset($_GET['jobDetailID'])) {
-
-    $jobDetailID = $_GET['jobDetailID'];
-
-} else {
-    header('location:adminJobList.php');
-}
-
-$getJobViewQuery = "SELECT jobdetail.jobDetailID,jobdetail.jobTitle, jobdetail.jobLocation, jobdetail.salaryRate, jobdetail.experienceLevel, jobdetail.jobIndustry, jobdetail.fullDescription, jobdetail.companyName, jobdetail.jobSkillsDescription, post.datePosted FROM `jobdetail` LEFT JOIN post ON jobdetail.jobDetailID = post.jobDetailID WHERE jobdetail.jobDetailID = '$jobDetailID';";
-
-$jobViewresult = executeQuery($getJobViewQuery);
-
-$jobTitle = '';
-$salaryRate = '';
-$expLevel = '';
-$companyName = '';
-$location = '';
-$industry = '';
-$skillRequirements = '';
-$jobDescription = '';
-$datePosted = '';
-
-while ($jobRow = mysqli_fetch_assoc($jobViewresult)) {
-
-    $jobDetailID = $jobRow['jobDetailID'];
-    $jobTitle = $jobRow['jobTitle'];
-    $salaryRate = $jobRow['salaryRate'];
-    $expLevel = $jobRow['experienceLevel'];
-    $companyName = $jobRow['companyName'];
-    $location = $jobRow['jobLocation'];
-    $industry = $jobRow['jobIndustry'];
-    $skillRequirements = $jobRow['jobSkillsDescription'];
-    $jobDescription = $jobRow['fullDescription'];
-    $datePosted = $jobRow['datePosted'];
-
-}
-
+include("../process/adminJobView2.php");
 ?>
 
 <!DOCTYPE html>
